477d7d08d4ff89e14cf1233b2bdfb2fd4e239748
veera <veera_cheedepudi-reddy@bio-rad.com>
2018-04-25 15:37:56 -0700

    fix: check kid file and call onDragEnter only if files

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,7 +14,11 @@ export function getDataTransferItems(event) {
     } else if (dt.items && dt.items.length) {
       // During the drag even the dataTransfer.files is null
       // but Chrome implements some drag store, which is accesible via dataTransfer.items
-      dataTransferItemsList = dt.items
+      for (let i = 0; i < dt.items.length; i += 1) {
+        if (dt.items[i].kind === 'file') {
+          dataTransferItemsList.push(dt.items[i])
+        }
+      }
     }
   } else if (event.target && event.target.files) {
     dataTransferItemsList = event.target.files
